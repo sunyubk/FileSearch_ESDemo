f57c097446a9489991a46923026923c7a7f0504d
孙宇 <1870971964@qq.com>
2023-10-12 09:25:06 +0800

    first commit

---
+++ src/main/java/com/sy/filesearch_esdemo/FileSearchEsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sy.filesearch_esdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSearchEsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSearchEsDemoApplication.class, args);
+    }
+
+}
